8fae55ecf867a5f967228dfb1730f0242aa51773
Pihu1998 <pranamika.pihu1998@gmail.com>
2024-04-07 15:09:54 +0100

    Renamed main.py to app.py and added doctrings to methods

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template, request
 from summarize import text_summarize
 from waitress import serve
 from flask_pymongo import PyMongo
-import time
+import time, datetime
 import os
 from os.path import join, dirname
 from dotenv import dotenv_values, load_dotenv
@@ -12,24 +12,45 @@ app = Flask(__name__)
 # Access MongoDB Access cluster
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
 # Setup PyMongo instance
 mongo = PyMongo(app)
+# Create mongo database
+db = mongo.db
 
 # Our initial form page
 @app.route('/')
 def app_ui():
+    """
+    This function renders the template for the text user interface.
+
+    Returns:
+        str: The rendered template for the text user interface.
+    """
     return render_template('text_ui.html')
 
 # Endpoint encapsulating AI logic
-@app.route('/summarize', methods=['POST'])
+@app.route('/sum', methods=['POST'])
 def summarize():
+    """
+    This function takes in a JSON object containing text and returns a summarized version of the text.
+
+    Args:
+        None
+
+    Returns:
+        A JSON object containing the summarized text.
+
+    Raises:
+        None
+    """
     data = request.json
     text = data.get('text', '')
     summary = text_summarize(text)
-    ts = time.time()
-    mongo.db.summary.insert_one({"timestamp": ts, "summarized": summary})
+    ts = datetime.datetime.now()
+    db.summary.insert_one({"timestamp": ts, "summarized": summary})
     return jsonify({'summary': summary})
 
 if __name__ == '__main__':
